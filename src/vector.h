faf7786b3116d45c1e7d39d57146492209bcf8c9
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2023-08-29 13:48:11 +0200

    introduce expression templates

--- src/vector.h
+++ src/vector.h
@@ -3,18 +3,21 @@
 
 #include <iostream>
 
+#include "expression.h"
+
+
 namespace ASC_bla
 {
   
   template <typename T>
-  class Vector
+  class Vector : public VectorView<T>
   {
-    size_t size_;
-    T * data_;
-    
+    typedef VectorView<T> BASE;
+    using BASE::size_;
+    using BASE::data_;
   public:
     Vector (size_t size) 
-      : size_(size), data_(new T[size]) { ; }
+      : VectorView<T> (size, new T[size]) { ; }
     
     Vector (const Vector & v)
       : Vector(v.Size())
@@ -23,14 +26,23 @@ namespace ASC_bla
     }
 
     Vector (Vector && v)
-      : size_{0}, data_(nullptr)
+      : VectorView<T> (0, nullptr)
     {
       std::swap(size_, v.size_);
       std::swap(data_, v.data_);
     }
 
-    ~Vector () { delete [] data_; }
+    template <typename TB>
+    Vector (const Expr<TB> & v)
+      : Vector(v.Size())
+    {
+      *this = v;
+    }
     
+    
+    ~Vector () { delete [] data_; }
+
+    using BASE::operator=;
     Vector & operator=(const Vector & v2)
     {
       for (size_t i = 0; i < size_; i++)
@@ -45,21 +57,9 @@ namespace ASC_bla
       return *this;
     }
     
-    size_t Size() const { return size_; }
-    T & operator()(size_t i) { return data_[i]; }
-    const T & operator()(size_t i) const { return data_[i]; }
   };
 
 
-  template <typename T>
-  Vector<T> operator+ (const Vector<T> & a, const Vector<T> & b)
-  {
-    Vector<T> sum(a.Size());
-    for (size_t i = 0; i < a.Size(); i++)
-      sum(i) = a(i)+b(i);
-    return sum;
-  }
-  
   template <typename T>
   std::ostream & operator<< (std::ostream & ost, const Vector<T> & v)
   {
