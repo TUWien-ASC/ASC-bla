98ddb1551d278d9a2fa66a738871be7847025820
Edoardo Bonetti <edoardo.bonetti@tuwien.ac.at>
2023-09-20 15:17:43 +0200

    now it works

--- src/matrix.h
+++ src/matrix.h
@@ -7,56 +7,39 @@ namespace ASC_bla {
 
 template <typename T>
 class Matrix {
+   private:
     size_t rows_;
     size_t cols_;
-    Vector<Vector<T>> data_;
+    // Vector<Vector<T>> data_;
+    T *data_;
 
    public:
     Matrix(size_t rows, size_t cols)
-        : rows_(rows), cols_(cols), data_(new Vector<T>[rows]) {
+        : rows_(rows), cols_(cols), data_(new T[rows * cols]) {
         ;
     }
 
-    // Copy constructor
     Matrix(const Matrix &m) : Matrix(m.SizeRows(), m.SizeCols()) { *this = m; }
 
-    // Move constructor
-    Matrix(Matrix &&m) : rows_{0}, cols_{0}, data_{Vector<T>(nullptr)} {
+    Matrix(Matrix &&m) : rows_{0}, cols_{0}, data_(nullptr) {
         std::swap(rows_, m.rows_);
         std::swap(cols_, m.cols_);
         std::swap(data_, m.data_);
     }
 
-    // Destructor
-    ~Matrix() { ; }   // delete[] data_; }
-
-    // Copy assignment operator
+    ~Matrix() { delete[] data_; }
     Matrix &operator=(const Matrix &m2) {
-        for (size_t i = 0; i < rows_; i++) {
-            for (size_t j = 0; j < cols_; j++) {
-                data_[i](j) = m2(i, j);
-            }
-        }
+        for (size_t i = 0; i < m2.cols_ * m2.rows_; i++) data_[i] = m2(i);
         return *this;
     }
-
-    // Move assignment operator
     Matrix &operator=(Matrix &&m2) {
-        for (size_t i = 0; i < rows_; i++) {
-            data_[i] = m2(i);
-        }
+        for (size_t i = 0; i < cols_ * rows_; i++) data_[i] = m2(i);
         return *this;
     }
 
     size_t SizeRows() const { return rows_; }
-
     size_t SizeCols() const { return cols_; }
 
-    Vector<T> &operator()(size_t i) { return data_(i); }
-
-    const Vector<T> &operator()(size_t i) const { return data_(i); }
-
-    // Matrix-vector multiplication
     Vector<T> operator*(const Vector<T> &v) const {
         if (cols_ != v.Size()) {
             throw std::invalid_argument(
@@ -66,7 +49,7 @@ class Matrix {
         Vector<T> result(rows_);
         for (size_t i = 0; i < rows_; i++) {
             for (size_t j = 0; j < cols_; j++) {
-                result(i) += data_[i](j) * v(j);
+                result(i) += data_[i + cols_ * j] * v(j);
             }
         }
 
@@ -82,15 +65,36 @@ class Matrix {
 
         Matrix result(rows_, cols_);
         for (size_t i = 0; i < rows_; i++) {
-            result(i) = data_(i) + m(i);
+            for (size_t j = 0; j < cols_; j++) {
+                result(i, j) = data_[i + cols_ * j] + m(i, j);
+            }
         }
 
         return result;
     }
 
     // operator () for element access
-    T &operator()(size_t i, size_t j) { return data_(i)(j); }
+    T &operator()(size_t i, size_t j) { return data_[i + cols_ * j]; }
+
+    const T &operator()(size_t i, size_t j) const {
+        return data_[i + cols_ * j];
+    }
 };
+
+template <typename T>
+std::ostream &
+operator<<(std::ostream &ost, const Matrix<T> &m) {
+    if (m.SizeRows() > 0 && m.SizeCols() > 0) {
+        for (size_t i = 0; i < m.SizeRows(); i++) {
+            ost << m(i, 0);
+            for (size_t j = 1; j < m.SizeCols(); j++) {
+                ost << ", " << m(i, j);
+            }
+            ost << std::endl;
+        }
+    }
+    return ost;
+}
 
 }   // namespace ASC_bla
 
--- tests/test_matrix.cc
+++ tests/test_matrix.cc
@@ -7,17 +7,31 @@ namespace bla = ASC_bla;
 
 int
 main() {
-    bla::Matrix<double> mat1(10, 10);
-    bla::Matrix<double> mat2(10, 10);
+    bla::Matrix<double> mat1(10, 9);
+    bla::Matrix<double> mat2(9, 10);
+    bla::Vector<double> vec1(10);
+
+    for (size_t i = 0; i < mat1.SizeRows(); i++) {
+        for (size_t j = 0; j < mat1.SizeCols(); j++) {
+            mat1(i, j) = (i + 1) * (j + 1);
+            mat2(i, j) = 0;
+            vec1(i) = 1;
+        }
+    }
+
+    // print mat1
 
     bla::Matrix<double> mat3 = mat1 + mat2;
+    std::cout << "mat3 = " << mat3 << std::endl;
 
-    for (int i = 0; i < mat3.SizeRows(); i++) {
-        for (int j = 0; j < mat3.SizeCols(); j++) {
-            std::cout << mat3(i, j) << ", ";
-        }
-        std::cout << std::endl;
-    }
+    bla::Vector<double> vec2 = mat1 * vec1;
+    std::cout << "vec2 = " << vec2 << std::endl;
+    // for (int i = 0; i < mat1.SizeRows(); i++) {
+    //     for (int j = 0; j < mat1.SizeCols(); j++) {
+    //         std::cout << mat1(i, j) << ", ";
+    //     }
+    //     std::cout << std::endl;
+    // }
 
     return 0;
 }
