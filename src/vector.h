37d76a99879327922a28929715fe07a5346e935f
Edoardo Bonetti <edoardo.bonetti@tuwien.ac.at>
2023-10-04 17:06:54 +0200

    changes

--- src/vector.h
+++ src/vector.h
@@ -3,57 +3,52 @@
 
 #include <iostream>
 
+#include "expression.h"
+
 namespace ASC_bla {
 
 template <typename T>
-class Vector {
-    size_t size_;
-    T* data_;
+class Vector : public VectorView<T> {
+  typedef VectorView<T> BASE;
+  using BASE::data_;
+  using BASE::size_;
 
-   public:
-    Vector(size_t size) : size_(size), data_(new T[size]) { ; }
+ public:
+  Vector(size_t size) : VectorView<T>(size, new T[size]) { ; }
 
-    Vector(const Vector& v) : Vector(v.Size()) { *this = v; }
+  Vector(const Vector& v) : Vector(v.Size()) { *this = v; }
 
-    Vector(Vector&& v) : size_{0}, data_(nullptr) {
-        std::swap(size_, v.size_);
-        std::swap(data_, v.data_);
-    }
+  Vector(Vector&& v) : VectorView<T>(0, nullptr) {
+    std::swap(size_, v.size_);
+    std::swap(data_, v.data_);
+  }
 
-    ~Vector() { delete[] data_; }
+  template <typename TB>
+  Vector(const Expr<TB>& v) : Vector(v.Size()) {
+    *this = v;
+  }
 
-    Vector& operator=(const Vector& v2) {
-        for (size_t i = 0; i < size_; i++) data_[i] = v2(i);
-        return *this;
-    }
+  ~Vector() { delete[] data_; }
 
-    Vector& operator=(Vector&& v2) {
-        for (size_t i = 0; i < size_; i++) data_[i] = v2(i);
-        return *this;
-    }
+  using BASE::operator=;
+  Vector& operator=(const Vector& v2) {
+    for (size_t i = 0; i < size_; i++) data_[i] = v2(i);
+    return *this;
+  }
 
-    size_t Size() const { return size_; }
-    T& operator()(size_t i) { return data_[i]; }
-    const T& operator()(size_t i) const { return data_[i]; }
+  Vector& operator=(Vector&& v2) {
+    for (size_t i = 0; i < size_; i++) data_[i] = v2(i);
+    return *this;
+  }
 };
 
 template <typename T>
-Vector<T>
-operator+(const Vector<T>& a, const Vector<T>& b) {
-    Vector<T> sum(a.Size());
-    for (size_t i = 0; i < a.Size(); i++) sum(i) = a(i) + b(i);
-    return sum;
+std::ostream& operator<<(std::ostream& ost, const Vector<T>& v) {
+  if (v.Size() > 0) ost << v(0);
+  for (size_t i = 1; i < v.Size(); i++) ost << ", " << v(i);
+  return ost;
 }
 
-template <typename T>
-std::ostream&
-operator<<(std::ostream& ost, const Vector<T>& v) {
-    if (v.Size() > 0)
-        ost << v(0);
-    for (size_t i = 1; i < v.Size(); i++) ost << ", " << v(i);
-    return ost;
-}
-
-}   // namespace ASC_bla
+}  // namespace ASC_bla
 
 #endif
--- tests/test_vector.cc
+++ tests/test_vector.cc
@@ -1,24 +1,26 @@
-#include <iostream>
-
 #include <vector.h>
+
+#include <iostream>
 
 namespace bla = ASC_bla;
 
-
-int main()
-{
-  size_t n = 5;
+int main() {
+  size_t n = 10;
   bla::Vector<double> x(n), y(n);
 
-  for (size_t i = 0; i < x.Size(); i++)
-    {
-      x(i) = i;
-      y(i) = 10;
-    }
+  for (size_t i = 0; i < x.Size(); i++) {
+    x(i) = i;
+    y(i) = 10;
+  }
 
-  bla::Vector<double> z = x+y;
-  
+  bla::Vector<double> z = x + y;
+
   std::cout << "x+y = " << z << std::endl;
+
+  std::cout << "type of (x+3*y) is  " << typeid(x + 3 * y).name() << std::endl;
+
+  x.Range(2, 9) = 3;
+  x.Slice(1, 5) = 10;
+
+  std::cout << "x = " << x << std::endl;
 }
-
-
