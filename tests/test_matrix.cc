bf48884faae8757713e71a0fdbe489701a5910fe
Edoardo Bonetti <edoardo.bonetti@tuwien.ac.at>
2023-10-12 13:19:53 +0200

    changes to prep for testing inv

--- tests/test_matrix.cc
+++ tests/test_matrix.cc
@@ -6,16 +6,28 @@ namespace bla = ASC_bla;
 
 int main() {
   size_t n = 10;
-  bla::Matrix<double, bla::ColMajor> x(n, n), y(n, n);
+  bla::Matrix<double, bla::RowMajor> x(n, n), y(n, n);
 
   for (size_t i = 0; i < x.SizeCols(); i++) {
     for (size_t j = 0; j < x.SizeRows(); j++) {
-      x(i, j) = i;
-      y(i, j) = 10;
+      x(i, j) = 10 * i + j;
+      y(i, i) = -2;
+      if ((i != 0) && (i != n - 1)) {
+        y(i, i - 1) = 1;
+        y(i - 1, i) = 1;
+        y(i, 1 + i) = 1;
+        y(i + 1, i) = 1;
+      }
     }
   }
+  y = 1;
+  // print the matrices
+  std::cout << "Print the initial matrices" << std::endl;
+  std ::cout << "x = " << x << std::endl;
+  std ::cout << "y = " << y << std::endl;
 
-  bla::Matrix<double, bla::ColMajor> z = x + y;
+  bla::Matrix<double, bla::RowMajor> z = x + y;
+  bla::Matrix<double, bla::ColMajor> w(n, n);
 
   std::cout << "x+y = " << z << std::endl;
   //
@@ -28,4 +40,10 @@ int main() {
   //// x.Slice(1, 5) = 10;
   //
   std::cout << "x = " << x << std::endl;
+  // std::cout << "x.Transpose()= " << Transpose(x) << std::endl;
+  std::cout << "x.Transpose()= " << Transpose(x) << std::endl;
+
+  w = Transpose(x);
+  // test x.Row(0)
+  std::cout << "x.Row(0) = " << x.Row(0) << std::endl;
 }
