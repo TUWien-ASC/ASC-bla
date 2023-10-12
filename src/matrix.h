6a56bae034c157a0a1c348c8aa05b3ff954d0755
Edoardo Bonetti <edoardo.bonetti@tuwien.ac.at>
2023-10-12 09:42:29 +0200

    put the deprecated code at the end

--- src/matrix.h
+++ src/matrix.h
@@ -1,3 +1,168 @@
+#ifndef FILE_MATRIX_H
+#define FILE_MATRIX_H
+
+#include <iostream>
+
+#include "expression.h"
+#include "vector.h"
+
+namespace ASC_bla {
+
+template <typename T, ORDERING ORD,
+          typename TDIST = std::integral_constant<size_t, 1>>
+class MatrixView : public MatExpr<MatrixView<T, ORD, TDIST>, ORD> {
+ protected:
+  T* data_;
+  size_t cols_;
+  size_t rows_;
+  TDIST dist_;
+
+ public:
+  MatrixView(size_t rows, size_t cols, T* data)
+      : data_(data), rows_(rows), cols_(cols) {}
+
+  MatrixView(size_t rows, size_t cols, TDIST dist, T* data)
+      : data_(data), rows_(rows), cols_(cols), dist_(dist) {}
+
+  template <typename TB>
+  MatrixView& operator=(const MatExpr<TB, ORD>& m2) {
+    for (size_t i = 0; i < rows_; i++) {
+      for (size_t j = 0; j < rows_; j++) {
+        data_[dist_ * i + j] = m2(i, j);
+      }
+    }
+    return *this;
+  }
+
+  MatrixView& operator=(T scal) {
+    for (size_t i = 0; i < cols_ * rows_; i++) data_[dist_ * i] = scal;
+    return *this;
+  }
+
+  auto View() const { return MatrixView(rows_, cols_, dist_, data_); }
+  size_t SizeCols() const { return cols_; }
+  size_t SizeRows() const { return rows_; }
+  T& operator()(size_t i, size_t j) { return data_[dist_ * i + j]; }
+  const T& operator()(size_t i, size_t j) const { return data_[dist_ * i + j]; }
+
+  auto Row(size_t i) const {
+    return VectorView<T, size_t>(cols_, dist_, data_ + i * dist_);
+  }
+
+  auto Col(size_t i) const {
+    return VectorView<T, size_t>(rows_, dist_, data_ + i * dist_);
+  }
+
+  auto Rows(size_t first, size_t next) const {
+    return MatrixView(next - first, cols_, dist_, data_ + first);
+  }
+
+  auto Cols(size_t first, size_t next) const {
+    return MatrixView(rows_, next - first, dist_, data_ + first);
+  }
+
+  auto Transpose() const { return MatrixView(cols_, rows_, dist_, data_); }
+
+  /*auto Inverse() const {
+  MatrixView<T, size_t> eye(rows_, cols_, 1, data_);
+  MatrixView<T, size_t> result(rows_, cols_, 1, data_);
+  for (size_t i = 0; i < rows_; i++) {
+    eye(i, i) = 1;
+  }
+
+  for (size_t i = 0; i < rows_; i++) {
+    T pivot = result(i, i);
+
+    // Make pivot element 1
+    for (size_t j = 0; j < cols_; j++) {
+      eye(i, j) = eye(i, j) / pivot;
+      result(i, j) = result(i, j) / pivot;
+    }
+
+    for (size_t k = 0; k < rows_; k++) {
+      if (k != i) {
+        T factor = result(k, i);
+        for (size_t j = 0; j < cols_; j++) {
+          eye(k, j) = eye(k, j) - factor * eye(i, j);
+          result(k, j) = result(k, j) - factor * result(i, j);
+        }
+      }
+    }
+  }
+  return eye;
+}*/
+};
+
+template <typename T, ORDERING ORD>
+class Matrix : public MatrixView<T, ORD> {
+  typedef MatrixView<T, ORD> BASE;
+  using BASE::cols_;
+  using BASE::data_;
+  using BASE::rows_;
+
+ public:
+  Matrix(size_t rows, size_t cols)
+      : MatrixView<T, ORD>(rows, cols, new T[rows * cols]) {
+    ;
+  }
+
+  Matrix(const Matrix& m) : Matrix(m.SizeRows(), m.SizeCols()) { *this = m; }
+
+  Matrix(Matrix&& m) : MatrixView<T, ORD>(0, 0, nullptr) {
+    std::swap(cols_, m.cols_);
+    std::swap(rows_, m.rows_);
+    std::swap(data_, m.data_);
+  }
+
+  template <typename TB>
+  Matrix(const MatExpr<TB, ORD>& m) : Matrix(m.SizeCols(), m.SizeRows()) {
+    *this = m;
+  }
+
+  ~Matrix() { delete[] data_; }
+
+  using BASE::operator=;
+  Matrix& operator=(const Matrix& m2) {
+    for (size_t i = 0; i < cols_; i++) {
+      for (size_t j = 0; j < rows_; j++) {
+        {
+          data_[i + cols_ * j] = m2(i, j);
+        }
+      }
+    }
+    return *this;
+  }
+
+  Matrix& operator=(Matrix&& m2) {
+    for (size_t i = 0; i < cols_; i++) {
+      for (size_t j = 0; j < rows_; j++) {
+        {
+          data_[i + cols_ * j] = m2(i, j);
+        }
+      }
+    }
+    return *this;
+  };
+};
+
+// output operator
+// How to proèperly implement this? using ...Args?
+template <typename T, ORDERING ORD>
+std::ostream& operator<<(std::ostream& os, const Matrix<T, ORD>& m) {
+  os << std::endl;
+  for (size_t i = 0; i < m.SizeRows(); i++) {
+    for (size_t j = 0; j < m.SizeCols(); j++) {
+      os << m(i, j) << " ";
+    }
+    os << std::endl;
+  }
+  return os;
+}
+
+}  // namespace ASC_bla
+
+#endif
+
 /*
 #ifndef FILE_MATRIX_H
 #define FILE_MATRIX_H
@@ -198,168 +363,3 @@ std::ostream &operator<<(std::ostream &os, const Matrix<T, ORD> &m) {
 #endif
 
 */
-
-#ifndef FILE_MATRIX_H
-#define FILE_MATRIX_H
-
-#include <iostream>
-
-#include "expression.h"
-#include "vector.h"
-
-namespace ASC_bla {
-
-template <typename T, ORDERING ORD,
-          typename TDIST = std::integral_constant<size_t, 1>>
-class MatrixView : public MatExpr<MatrixView<T, ORD, TDIST>, ORD> {
- protected:
-  T* data_;
-  size_t cols_;
-  size_t rows_;
-  TDIST dist_;
-
- public:
-  MatrixView(size_t rows, size_t cols, T* data)
-      : data_(data), rows_(rows), cols_(cols) {}
-
-  MatrixView(size_t rows, size_t cols, TDIST dist, T* data)
-      : data_(data), rows_(rows), cols_(cols), dist_(dist) {}
-
-  template <typename TB>
-  MatrixView& operator=(const MatExpr<TB, ORD>& m2) {
-    for (size_t i = 0; i < rows_; i++) {
-      for (size_t j = 0; j < rows_; j++) {
-        data_[dist_ * i + j] = m2(i, j);
-      }
-    }
-    return *this;
-  }
-
-  MatrixView& operator=(T scal) {
-    for (size_t i = 0; i < cols_ * rows_; i++) data_[dist_ * i] = scal;
-    return *this;
-  }
-
-  auto View() const { return MatrixView(rows_, cols_, dist_, data_); }
-  size_t SizeCols() const { return cols_; }
-  size_t SizeRows() const { return rows_; }
-  T& operator()(size_t i, size_t j) { return data_[dist_ * i + j]; }
-  const T& operator()(size_t i, size_t j) const { return data_[dist_ * i + j]; }
-
-  auto Row(size_t i) const {
-    return VectorView<T, size_t>(cols_, dist_, data_ + i * dist_);
-  }
-
-  auto Col(size_t i) const {
-    return VectorView<T, size_t>(rows_, dist_, data_ + i * dist_);
-  }
-
-  auto Rows(size_t first, size_t next) const {
-    return MatrixView(next - first, cols_, dist_, data_ + first);
-  }
-
-  auto Cols(size_t first, size_t next) const {
-    return MatrixView(rows_, next - first, dist_, data_ + first);
-  }
-
-  auto Transpose() const { return MatrixView(cols_, rows_, dist_, data_); }
-
-  /*auto Inverse() const {
-  MatrixView<T, size_t> eye(rows_, cols_, 1, data_);
-  MatrixView<T, size_t> result(rows_, cols_, 1, data_);
-  for (size_t i = 0; i < rows_; i++) {
-    eye(i, i) = 1;
-  }
-
-  for (size_t i = 0; i < rows_; i++) {
-    T pivot = result(i, i);
-
-    // Make pivot element 1
-    for (size_t j = 0; j < cols_; j++) {
-      eye(i, j) = eye(i, j) / pivot;
-      result(i, j) = result(i, j) / pivot;
-    }
-
-    for (size_t k = 0; k < rows_; k++) {
-      if (k != i) {
-        T factor = result(k, i);
-        for (size_t j = 0; j < cols_; j++) {
-          eye(k, j) = eye(k, j) - factor * eye(i, j);
-          result(k, j) = result(k, j) - factor * result(i, j);
-        }
-      }
-    }
-  }
-  return eye;
-}*/
-};
-
-template <typename T, ORDERING ORD>
-class Matrix : public MatrixView<T, ORD> {
-  typedef MatrixView<T, ORD> BASE;
-  using BASE::cols_;
-  using BASE::data_;
-  using BASE::rows_;
-
- public:
-  Matrix(size_t rows, size_t cols)
-      : MatrixView<T, ORD>(rows, cols, new T[rows * cols]) {
-    ;
-  }
-
-  Matrix(const Matrix& m) : Matrix(m.SizeRows(), m.SizeCols()) { *this = m; }
-
-  Matrix(Matrix&& m) : MatrixView<T, ORD>(0, 0, nullptr) {
-    std::swap(cols_, m.cols_);
-    std::swap(rows_, m.rows_);
-    std::swap(data_, m.data_);
-  }
-
-  template <typename TB>
-  Matrix(const MatExpr<TB, ORD>& m) : Matrix(m.SizeCols(), m.SizeRows()) {
-    *this = m;
-  }
-
-  ~Matrix() { delete[] data_; }
-
-  using BASE::operator=;
-  Matrix& operator=(const Matrix& m2) {
-    for (size_t i = 0; i < cols_; i++) {
-      for (size_t j = 0; j < rows_; j++) {
-        {
-          data_[i + cols_ * j] = m2(i, j);
-        }
-      }
-    }
-    return *this;
-  }
-
-  Matrix& operator=(Matrix&& m2) {
-    for (size_t i = 0; i < cols_; i++) {
-      for (size_t j = 0; j < rows_; j++) {
-        {
-          data_[i + cols_ * j] = m2(i, j);
-        }
-      }
-    }
-    return *this;
-  };
-};
-
-// output operator
-// How to proèperly implement this? using ...Args?
-template <typename T, ORDERING ORD>
-std::ostream& operator<<(std::ostream& os, const Matrix<T, ORD>& m) {
-  os << std::endl;
-  for (size_t i = 0; i < m.SizeRows(); i++) {
-    for (size_t j = 0; j < m.SizeCols(); j++) {
-      os << m(i, j) << " ";
-    }
-    os << std::endl;
-  }
-  return os;
-}
-
-}  // namespace ASC_bla
-
-#endif
