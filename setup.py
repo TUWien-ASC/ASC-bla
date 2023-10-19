b864d5d4339fafb9830752d09b7ce06556091b28
Edoardo Bonetti <bonettiedo@gmail.com>
2023-10-19 17:10:03 +0200

    Added author

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from skbuild import setup
 setup(
     name="ASCsoft",
     version="0.0.2",
-    author="J. Schoeberl",
+    author="J. Schoeberl, E. Bonetti",
     license="MIT",
     packages=["ASCsoft"],
     cmake_args=[]
