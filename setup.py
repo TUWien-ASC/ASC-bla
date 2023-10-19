21d57f32c2bd93671b03da1625cecb5e8ba14d1b
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2023-10-19 13:52:13 +0200

    set cmake-args in setup.py for MinGW generator

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from skbuild import setup
 
+import sys
+_cmake_args = []
+
+if 'win32' in sys.platform:
+    _cmake_args += ['-G', 'MinGW Makefiles']
+
+    
 setup(
     name="ASCsoft",
     version="0.0.2",
     author="J. Schoeberl",
     license="MIT",
     packages=["ASCsoft"],
-    cmake_args=[]
+    cmake_args=_cmake_args
 )
