628121c7cb827fb2e253c886e7c206d830b0ff33
Edoardo Bonetti <edoardo.bonetti@tuwien.ac.at>
2023-10-23 17:53:38 +0200

    test for vector

--- py_tests/test_vector.py
+++ py_tests/test_vector.py
@@ -11,6 +11,7 @@ x[:] = 1
 print ("x =", x)
 n = 10
 X = Matrix(n,n)
+Y = Matrix(n,n)
 for i in range(n):
     for j in range(n):
         X[i,j] = 10*i + j
@@ -30,5 +31,55 @@ print((X))
 x = Vector(10)
 for i in range(10):
     x[i] = 10
-x[0:10:4] = 3
+x[0:4] = 3
 print (x)
+
+X=-2*X
+
+#print(X[0:3, 0:3]* X[0, 0:3])
+
+# Measure timings for Matrix-Matrix multiplicaton called from Python 
+# (width=height=n, with n=10, n=100, n=1000). 
+# Split times into actual C++ computations, and overhead due to Python wrapping.
+# Compare with Numpy timings in milliseconds.
+
+import time
+import numpy as np
+
+T_cpp = []
+T_np = []
+
+for n in [100*n for n in range(1,14)]:
+
+    X = Matrix(n,n)
+    Y = Matrix(n,n)
+    for i in range(n):
+        for j in range(n):
+            X[i,j] = 10*i + j
+            Y[i,j] = 10*i + j
+
+    t0 = time.time()
+    Z = X*Y
+    t1 = time.time()
+    print ("n = %4d: My C++ time = %6.3f ms" % (n, (t1-t0)))
+    T_cpp.append(t1-t0)
+    
+
+    Xnp = np.asarray(X)
+    Ynp = np.asarray(Y)
+    t2 = time.time()
+    Znp = np.dot(Xnp, Ynp)
+    t3 = time.time()
+    print ("n = %4d: Numpy time = %6.3f ms" % (n, (t3-t2)))
+    T_np.append(t3-t2)
+
+# Plot timings log scale
+import matplotlib.pyplot as plt
+plt.figure()
+plt.plot(np.log(T_cpp), label="C++")
+plt.plot(np.log(T_np), label="Numpy")
+plt.legend()
+plt.xlabel("n")
+plt.ylabel("time [s]")
+plt.title("Matrix-Matrix multiplication")
+plt.show()
